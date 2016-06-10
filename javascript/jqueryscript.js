fef7272080d2d1ecb395dbd25752122bc35bfbf1
Josef Zacek <info@josefzacek.com>
2016-06-11 00:00:14 +0100

    updated to match change to bootstrap

--- javascript/jqueryscript.js
+++ javascript/jqueryscript.js
@@ -1,20 +1,14 @@
-// JavaScript Document
-
-
 $(document).ready(function(){
 	// load index page
-	$('article').load('pages/index.php');
-	
+	$('.wrapper').load('pages/index.php');
+
 	// linking pages
 	$('nav ul li a').on('click', navigate);
-	
+
 	function navigate(e) {
 		e.preventDefault();
 		var page = $(this).attr('href');
-		$('article').load('pages/'+ page +'.php');	
+		$('.wrapper').load('pages/'+ page +'.php');
 	};
-	
-	
-	
-		   
+
 });
