552b7adb4d734f251cf9d5c4091c5ecfd6491bee
Josef Zacek <info@josefzacek.com>
2016-06-13 00:15:52 +0100

    main content for index page

--- pages/index.php
+++ pages/index.php
@@ -8,4 +8,28 @@
   </p>
 </div>
 
+<h1 class="text-center">What's behind this page</h1>
+<hr>
+
+<div class="row">
+  <div class="col-md-4">
+    <img src="images/php-logo.png" class="img-responsive center-block" alt="PHP">
+    <hr>
+    <p><strong>PHP</strong> is a server-side scripting language designed for web development but also used as a general-purpose programming language. Originally created by Rasmus Lerdorf in 1994, the PHP reference implementation is now produced by The PHP Group. PHP originally stood for Personal Home Page, but it now stands for the recursive backronym PHP: Hypertext Preprocessor</p>
+    <p><a class="btn btn-primary" href="http://php.net/" target="_blank" role="button"><i class="fa fa-external-link" aria-hidden="true"></i> View more »</a></p>
+  </div>
+  <div class="col-md-4">
+    <img src="images/jquery-logo.png" class="img-responsive center-block" alt="jQuery">
+    <hr>
+    <p><strong>jQuery</strong> is a cross-platform JavaScript library designed to simplify the client-side scripting of HTML. jQuery is the most popular JavaScript library in use today, with installation on 65% of the top 10 million highest-trafficked sites on the Web. jQuery is free, open-source software licensed under the MIT License.</p>
+    <p><a class="btn btn-primary" href="https://jquery.com/" target="_blank" role="button"><i class="fa fa-external-link" aria-hidden="true"></i> View more »</a></p>
+ </div>
+  <div class="col-md-4">
+    <img src="images/bootstrap-logo.png" class="img-responsive center-block" alt="Bootstrap">
+    <hr>
+    <p><strong>Bootstrap</strong> is a free and open-source front-end web framework for designing websites and web applications. It contains HTML- and CSS-based design templates for typography, forms, buttons, navigation and other interface components, as well as optional JavaScript extensions. Unlike many web frameworks, it concerns itself with front-end development only.</p>
+    <p><a class="btn btn-primary" href="http://getbootstrap.com/" target="_blank" role="button"><i class="fa fa-external-link" aria-hidden="true"></i> View more »</a></p>
+  </div>
+</div>
+
 
