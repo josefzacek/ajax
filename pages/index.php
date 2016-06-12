5b6a5cc52feb4f3a0a5bde563f466a0600d3c652
Josef Zacek <info@josefzacek.com>
2016-06-13 00:13:45 +0100

    index page jumbotron text update

--- pages/index.php
+++ pages/index.php
@@ -1,5 +1,6 @@
 <div class="jumbotron text-center">
-  <h1>Simple example of Ajax</h1> 
+  <h1>Simple example of Ajax</h1>
+  <p>Update a web page without reloading the page</p>
   <p>
     <a href="https://github.com/josefzacek/ajax" target="_blank" class="btn btn-default">
       <i class="fa fa-github fa-lg"></i> View Source on GitHub
