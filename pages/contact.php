40de76cb43fe35f13863b0c4d2b37646ec6a1720
Josef Zacek <info@josefzacek.com>
2016-06-16 23:12:29 +0100

    contact page front-end

--- pages/contact.php
+++ pages/contact.php
@@ -2,5 +2,59 @@
 
 <div id="google-map-canvas"></div>
     
+<div class="row">
 
+  <div class="col-lg-8 col-lg-offset-2">
+    <h1 class="text-center">Contact us</h1>
+    <hr>
 
+    <form id="contact-form" method="post" action="mailer.php">
+
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label for="form-name">Firstname*</label>
+            <input id="form-name" type="text" name="firstname" class="form-control" required>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="form-group">
+            <label for="form-lastname">Lastname*</label>
+            <input id="form-lastname" type="text" name="lastname" class="form-control" required>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label for="form-email">Email*</label>
+            <input id="form-email" type="email" name="email" class="form-control" required>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="form-group">
+            <label for="form-phone">Phone*</label>
+            <input id="form-phone" type="text" name="phone" class="form-control" required>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12">
+          <div class="form-group">
+            <label for="form-message">Message*</label>
+            <textarea id="form-message" name="message" class="form-control" rows="10" required></textarea>
+          </div>
+        </div>
+      </div>
+
+      <button type="submit" id="send-message" class="btn btn-primary pull-right">Send Message</button>
+
+      <p class="text-danger"><strong>*</strong> Required fields</p>
+
+    </form>
+
+  </div>
+
+</div>
