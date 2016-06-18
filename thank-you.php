fd6fca216955dc85a38154763e758aae88e0f28c
Josef Zacek <info@josefzacek.com>
2016-06-18 23:37:24 +0100

    show message if email is successfully sent

--- thank-you.php
+++ thank-you.php
@@ -42,6 +42,45 @@
       'text' => $email_content
     ));
     
+    // Send the email
+    if($mailgun_result->http_response_code == 200) {
+      echo '<!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>Ajax</title>
+            <link rel="shortcut icon" href="images/favicon.ico"/>
+
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+            <link rel="stylesheet" type="text/css" href="style.css">
+
+            <script src="https://use.fontawesome.com/ab39f92078.js"></script>
+
+            <!--[if lt IE 9]>
+              <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+              <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+            <![endif]-->
+
+          </head>
+          <body>
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                  <p>&nbsp;</p>
+                  <p>&nbsp;</p>
+                  <h2>Thank You!</br> Your message has been sent.</h2>
+                  <p>
+                    <a class="btn btn-primary" href="/" role="button">Home page</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </body>
+        </html>';
+    } 
   }
   else {
     // Redirect to 404 page
