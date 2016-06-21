d84b843d49187f5a74d1e237e8e5321d112b199a
Josef Zacek <info@josefzacek.com>
2016-06-21 21:45:58 +0100

    gallery page filled out with images, lazy load and lightbox working

--- pages/gallery.php
+++ pages/gallery.php
@@ -1,69 +1,197 @@
 <div class="row">
 
   <div class="col-lg-12">
-      <h1 class="page-header">Thumbnail Gallery</h1>
+      <h1 class="page-header">Gallery</h1>
+      <p>Click on image to enlarge</p>
+      <p>Images outside of viewport are not loaded until user scrolls to them. Images are provided from <a href="https://unsplash.com/" target="_blank">unsplash.com</a> )</p>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-      <a class="thumbnail" href="#">
-          <img class="img-responsive lazy" data-original="https://source.unsplash.com/lSegRSDBMLw/400x300" alt="">
-      </a>
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/lSegRSDBMLw" data-lightbox="gallery">
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/lSegRSDBMLw/400x300" alt="">
+    </a>
   </div>
-  <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-      <a class="thumbnail" href="#">
-          <img class="img-responsive lazy" data-original="https://source.unsplash.com/Bkci_8qcdvQ/400x300" alt="">
-      </a>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/Bkci_8qcdvQ" data-lightbox="gallery">
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/Bkci_8qcdvQ/400x300" alt="">
+    </a>
   </div>
-  <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-      <a class="thumbnail" href="#">
-          <img class="img-responsive lazy" data-original="https://source.unsplash.com/A88emaZe7d8/400x300" alt="">
-      </a>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/A88emaZe7d8" data-lightbox="gallery">
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/A88emaZe7d8/400x300" alt="">
+    </a>
   </div>
-  <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-      <a class="thumbnail" href="#">
-          <img class="img-responsive lazy" data-original="https://source.unsplash.com/rewyZqUwAqY/400x300" alt="">
-      </a>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/rewyZqUwAqY" data-lightbox="gallery">
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/rewyZqUwAqY/400x300" alt="">
+    </a>
   </div>
-  <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-      <a class="thumbnail" href="#">
-          <img class="img-responsive lazy" data-original="https://source.unsplash.com/3JZeW3rtg6E/400x300" alt="">
-      </a>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/3JZeW3rtg6E" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/3JZeW3rtg6E/400x300" alt="">
+    </a>
   </div>
-  <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-      <a class="thumbnail" href="#">
-          <img class="img-responsive lazy" data-original="https://source.unsplash.com/B0n4-PeF60Q/400x300" alt="">
-      </a>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/B0n4-PeF60Q" data-lightbox="gallery">
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/B0n4-PeF60Q/400x300" alt="">
+    </a>
   </div>
-  <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-      <a class="thumbnail" href="#">
-          <img class="img-responsive lazy" data-original="https://source.unsplash.com/sOPbEr1ym3k/400x300" alt="">
-      </a>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/n3XTxxV7qhI" data-lightbox="gallery">
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/n3XTxxV7qhI/400x300" alt="">
+    </a>
   </div>
-  <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-      <a class="thumbnail" href="#">
-          <img class="img-responsive lazy" data-original="https://source.unsplash.com/WTAPz7GsLeE/400x300" alt="">
-      </a>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/AQ8jQo7QULU" data-lightbox="gallery">
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/AQ8jQo7QULU/400x300" alt="">
+    </a>
   </div>
-  <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-      <a class="thumbnail" href="#">
-          <img class="img-responsive lazy" data-original="https://source.unsplash.com/5QAeUF6Pduc/400x300" alt="">
-      </a>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/5QAeUF6Pduc" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/5QAeUF6Pduc/400x300" alt="">
+    </a>
   </div>
-  <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-      <a class="thumbnail" href="#">
-          <img class="img-responsive lazy" src="http://placehold.it/400x300" alt="">
-      </a>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/MPhTBGi9pNA" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/MPhTBGi9pNA/400x300" alt="">
+    </a>
   </div>
-  <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-      <a class="thumbnail" href="#">
-          <img class="img-responsive lazy" src="http://placehold.it/400x300" alt="">
-      </a>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/IOCN-gwfwEU" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/IOCN-gwfwEU/400x300" alt="">
+    </a>
   </div>
-  <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-      <a class="thumbnail" href="#">
-          <img class="img-responsive lazy" src="http://placehold.it/400x300" alt="">
-      </a>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/8OPIIY4Jzps" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/8OPIIY4Jzps/400x300" alt="">
+    </a>
   </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/YYGlIKr9G4k" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/YYGlIKr9G4k/400x300" alt="">
+    </a>
+  </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/W172-ieBK7s" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/W172-ieBK7s/400x300" alt="">
+    </a>
+  </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/T16-cG1gEdo" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/T16-cG1gEdo/400x300" alt="">
+    </a>
+  </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/OzXFhQIVyGY" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/OzXFhQIVyGY/400x300" alt="">
+    </a>
+  </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/b82R8cnxfZg" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/b82R8cnxfZg/400x300" alt="">
+    </a>
+  </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/wLn_6Y4j7EY" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/wLn_6Y4j7EY/400x300" alt="">
+    </a>
+  </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/fw7lR3ibfpU" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/fw7lR3ibfpU/400x300" alt="">
+    </a>
+  </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/vmgeBtLPQx8" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/vmgeBtLPQx8/400x300" alt="">
+    </a>
+  </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/swPTbY7v6Ow" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/swPTbY7v6Ow/400x300" alt="">
+    </a>
+  </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/gtaDpWGyi24" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/gtaDpWGyi24/400x300" alt="">
+    </a>
+  </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/c9AuglIqhes" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/c9AuglIqhes/400x300" alt="">
+    </a>
+  </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/Yr5kaJbOm_g" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/Yr5kaJbOm_g/400x300" alt="">
+    </a>
+  </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/dQXXN0cvZnI" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/dQXXN0cvZnI/400x300" alt="">
+    </a>
+  </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/J_c_aVesGpk" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/J_c_aVesGpk/400x300" alt="">
+    </a>
+  </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/E-Z9mC0PaGw" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/E-Z9mC0PaGw/400x300" alt="">
+    </a>
+  </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/ZW7unsyfPGQ" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/ZW7unsyfPGQ/400x300" alt="">
+    </a>
+  </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/Jky9coxzeHI" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/Jky9coxzeHI/400x300" alt="">
+    </a>
+  </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/1cWZgnBhZRs" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/1cWZgnBhZRs/400x300" alt="">
+    </a>
+  </div>
+
+  <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="https://source.unsplash.com/9JBO0HLm1Pc" data-lightbox="gallery"> 
+      <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/9JBO0HLm1Pc/400x300" alt="">
+    </a>
+  </div>
+
 
 </div>
 
