8d591697adf5af54f5b6c0c4e8f24f9126775335
Josef Zacek <info@josefzacek.com>
2016-06-28 23:51:48 +0100

    content for about page

--- pages/about.php
+++ pages/about.php
@@ -1,7 +1,81 @@
-<h1>About</h1>
+<h1 class="page-header">What is Ajax</h1>
 
+<div class="row">
 
+  <div class="col-xs-12 col-sm-12 col-md-4 col-lg-6 pull-right">
+    <a href="images/ajax-model.jpg" data-lightbox="gallery">
+      <img  src="images/ajax-model.jpg" alt="Ajax model" class="img-responsive center-block">
+    </a>
+  </div>
 
+  <div class="col-xs-12 col-sm-12 col-md-8 col-lg-6">
 
+    <p><b>Ajax in a Nutshell</b></p>
 
+    <p>A way to hit your server and get some data from it without making your poor user reload the whole page. (You can send data to the server, too, such as form submissions.) The HTTP request is normal, just done from Javascript! And no reloads!</p>
+
+    <p><b>Ajax description</b></p>
+    <ul>
+      <li>AJAX stands for Asynchronous JavaScript And XML</li>
+      <li>Allows web pages or parts of them to be updated asynchronously</li>
+      <li>Based on XML HTTP request object (AKA XHR)</li>
+      <li>Requires basic understanding of:
+        <ul>
+          <li>(X)HTML – displaying the data</li>
+          <li>CSS – styling the data</li>
+          <li>JavaScript – manipulating the DOM</li>
+          <li>XML / JSON – received / sent data format (also HTML & text)</li>
+        </ul>
+      </li>
+    </ul>
+
+    <p><b>Ajax usage</b></p>
+    <ul>
+      <li>Update a web page without reloading the page</li>
+      <li>Request data from a server - after the page has loaded</li>
+      <li>Receive data from a server - after the page has loaded</li>
+      <li>Send data to a server - in the background</li>
+    </ul>
+  </div>
+</div>
+
+<hr>
+
+<div class="row">
+  <div class="col-md-6">
+    <p><b>Ajax Pros</b></p>
+    <ul>
+      <li>Better interactivity &amp; responsiveness</li>
+      <li>Impressive UX</li>
+      <li>Reduced connections to the web server (partial rendering)</li>
+      <li>Reduced bandwidth</li>
+    </ul>
+  </div>
+
+  <div class="col-md-6">
+    <p><b>Ajax Cons</b></p>
+    <ul>
+      <li>Bookmarking is useless</li>
+      <li>Requires JavaScript to be enabled on browsers</li>
+      <li>Unfriendly for SEO &amp; analytics </li>
+      <li>Unfriendly for screen readers</li>
+    </ul>
+  </div>
+</div>
+
+<hr>
+
+<div class="row">
+  <div class="col-md-12">
+    <p><b>Resources and Tutorials</b></p>
+    <ul>
+      <li><a href="http://api.jquery.com/category/ajax/" target="_blank">http://api.jquery.com/category/ajax</a></li>
+      <li><a href="http://www.w3schools.com/ajax/" target="_blank">http://www.w3schools.com/ajax</a></li>
+      <li><a href="https://developer.mozilla.org/en/docs/AJAX">https://developer.mozilla.org/en/docs/AJAX</a></li>
+      <li><a href="https://www.sitepoint.com/guide-vanilla-ajax-without-jquery/" target="_blank">https://www.sitepoint.com/guide-vanilla-ajax-without-jquery</a></li>
+      <li><a href="https://www.sitepoint.com/use-jquerys-ajax-function/" target="_blank">https://www.sitepoint.com/use-jquerys-ajax-function</a></li>
+      <li><a href="https://learn.jquery.com/ajax/" target="_blank">https://learn.jquery.com/ajax</a></li>
+    </ul>
+  </div>
+</div>
 
