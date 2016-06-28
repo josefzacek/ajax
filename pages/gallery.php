ff6780d92cb500e8cca8d9b5d038acb6deaa1b7c
Josef Zacek <info@josefzacek.com>
2016-06-28 23:54:24 +0100

    change order of bootstrap classes

--- pages/gallery.php
+++ pages/gallery.php
@@ -6,325 +6,325 @@
       <p>Images outside of viewport are not loaded until user scrolls to them. Images provided by <a href="https://unsplash.com/" target="_blank">unsplash.com</a> </p>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/lSegRSDBMLw" data-lightbox="gallery">
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/lSegRSDBMLw/300x300" alt="People walking">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/Bkci_8qcdvQ" data-lightbox="gallery">
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/Bkci_8qcdvQ/300x300" alt="Mountain view">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/A88emaZe7d8" data-lightbox="gallery">
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/A88emaZe7d8/300x300" alt="People sitting on a bench">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/rewyZqUwAqY" data-lightbox="gallery">
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/rewyZqUwAqY/300x300" alt="Walking on train track">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/3JZeW3rtg6E" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/3JZeW3rtg6E/300x300" alt="Skateboarding">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/B0n4-PeF60Q" data-lightbox="gallery">
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/B0n4-PeF60Q/300x300" alt="Headphones">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/n3XTxxV7qhI" data-lightbox="gallery">
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/n3XTxxV7qhI/300x300" alt="Lake view">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/AQ8jQo7QULU" data-lightbox="gallery">
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/AQ8jQo7QULU/300x300" alt="Raw sea">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/5QAeUF6Pduc" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/5QAeUF6Pduc/300x300" alt="Car rear light">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/MPhTBGi9pNA" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/MPhTBGi9pNA/300x300" alt="Grass">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/IOCN-gwfwEU" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/IOCN-gwfwEU/300x300" alt="Paraglide pilot">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/8OPIIY4Jzps" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/8OPIIY4Jzps/300x300" alt="Skyscrapper">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/YYGlIKr9G4k" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/YYGlIKr9G4k/300x300" alt="Flower">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/W172-ieBK7s" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/W172-ieBK7s/300x300" alt="Photo camera">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/T16-cG1gEdo" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/T16-cG1gEdo/300x300" alt="Light effect">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/OzXFhQIVyGY" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/OzXFhQIVyGY/300x300" alt="Sculpture">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/b82R8cnxfZg" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/b82R8cnxfZg/300x300" alt="Trees">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/wLn_6Y4j7EY" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/wLn_6Y4j7EY/300x300" alt="Winter city">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/fw7lR3ibfpU" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/fw7lR3ibfpU/300x300" alt="Ruler and paper">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/vmgeBtLPQx8" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/vmgeBtLPQx8/300x300" alt="Bike">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/swPTbY7v6Ow" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/swPTbY7v6Ow/300x300" alt="Person standing">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/gtaDpWGyi24" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/gtaDpWGyi24/300x300" alt="Shoes">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/c9AuglIqhes" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/c9AuglIqhes/300x300" alt="Bags">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/Yr5kaJbOm_g" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/Yr5kaJbOm_g/300x300" alt="Desert and coffee">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/dQXXN0cvZnI" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/dQXXN0cvZnI/300x300" alt="Coast view">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/J_c_aVesGpk" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/J_c_aVesGpk/300x300" alt="Sky">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/E-Z9mC0PaGw" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/E-Z9mC0PaGw/300x300" alt="Birds talking">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/ZW7unsyfPGQ" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/ZW7unsyfPGQ/300x300" alt="London Eye">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/Jky9coxzeHI" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/Jky9coxzeHI/300x300" alt="Window flowers">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/1cWZgnBhZRs" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/1cWZgnBhZRs/300x300" alt="Exotic fruit">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/9JBO0HLm1Pc" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/9JBO0HLm1Pc/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/VBNb52J8Trk" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/VBNb52J8Trk/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/_14v_Fbk4SQ" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/_14v_Fbk4SQ/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/oTQVwECws8o" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/oTQVwECws8o/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/Q47I_dCsNyY" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/Q47I_dCsNyY/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/B4DOEDHEpMQ" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/B4DOEDHEpMQ/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/qH36EgNjPJY" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/qH36EgNjPJY/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/kKxfELd_5pg" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/kKxfELd_5pg/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/Lzx4J_Pb3sk" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/Lzx4J_Pb3sk/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/c8TWWQ5ZnUw" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/c8TWWQ5ZnUw/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/TZCehSn-T-o" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/TZCehSn-T-o/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/Fo5dTm6ID1Y" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/Fo5dTm6ID1Y/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/sGblr5yVXiM" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/sGblr5yVXiM/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/ea9Zg7HkFXQ" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/ea9Zg7HkFXQ/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/7pGehyH7o64" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/7pGehyH7o64/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/bITjK6W2Alw" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/bITjK6W2Alw/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/aIYFR0vbADk" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/aIYFR0vbADk/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/jq3BQshrzVQ" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/jq3BQshrzVQ/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/WgGJjGN4_ck" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/WgGJjGN4_ck/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/h13Y8vyIXNU" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/h13Y8vyIXNU/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/JzkgpML_8XI" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/JzkgpML_8XI/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/OJJIaFZOeX4" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/OJJIaFZOeX4/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/sseiVD2XsOk" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/sseiVD2XsOk/300x300" alt="Lady by river">
     </a>
   </div>
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
+  <div class="col-xs-6 col-md-4 col-lg-3">
     <a href="https://source.unsplash.com/5Qwz2KyfIBE" data-lightbox="gallery"> 
       <img class="thumbnail img-responsive lazy" data-original="https://source.unsplash.com/5Qwz2KyfIBE/300x300" alt="Lady by river">
     </a>
